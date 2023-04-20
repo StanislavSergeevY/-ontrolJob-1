f3a2bbf00d7950306c6cd1abc9083603183f57ee
StanislavSergeev <nelson-uo@mail.ru>
2023-04-21 02:29:38 +0300

    Добавил альтернативный вариант решения, без использование итераций и рекурсий не для зачёта

---
+++ ControlJobAlternative/Program.cs
@@ -0,0 +1,28 @@
+﻿// ########################################################################
+// # !!! Решение не для зачёта, просто альтернативный вариант решения !!! #
+// # !!! без использование итераций и рекурсий !!!                        #
+// ########################################################################
+
+Console.Clear(); Console.Write("Введите значения массива через пробел: ");
+string[] ArrayFirst = Console.ReadLine().Split(" ").ToArray();
+int i = 0;
+
+foreach (string elem in ArrayFirst)
+{
+  if (elem.Length <= 3) i++;
+}
+
+string[] ArraySecond = new string [i];
+i = 0;
+
+foreach (string elem in ArrayFirst)
+{
+  if (elem.Length <= 3)
+  {
+    ArraySecond[i] = elem;
+    i++;
+  }
+}
+
+Console.WriteLine($"Creation ArrayFirst complied - [{string.Join(", ", ArrayFirst)}]");
+Console.WriteLine($"Creation ArraySecond complied - [{string.Join(", ", ArraySecond)}]");
